fb99ea72e6a4963beb7ce4b86d8e1f00d57360d9
Modar Husseıno <modar-eagle90@outlook.com>
2021-05-29 20:28:16 +0300

    Add project files.

---
+++ BussinesLayer/CategoryManager.cs
@@ -0,0 +1,30 @@
+﻿using DataAccesLayer.Concrete.Repositories;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinesLayer
+{
+    public class CategoryManager
+    {
+        GenericRepository<Category> repo = new GenericRepository<Category>();
+        public List<Category> GetAllBL()
+        {
+            return repo.List();
+        }
+        public void CategoryAddBL(Category p)
+        {
+            if(p.CategoryName=="" || p.CategoryName.Length<=3 || p.CategoryDescription=="" || p.CategoryName.Length >= 51)
+            {
+                //hata mesaji
+            }
+            else
+            {
+                repo.Insert(p);
+            }
+        }
+    }
+}
